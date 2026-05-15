d71184509987c55a7f7134c5354f1224f85d726d
sfulmer <sfulmer@redhat.com>
2026-05-15 14:47:45 -0400

    Expand to 50 modules for full platform coverage

---
+++ plugins/modules/newrelic_browser_app.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2026 Steve Fulmer
+# Apache-2.0 (see LICENSE)
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""newrelic_browser_app module."""
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: newrelic_browser_app
+short_description: Manage New Relic Browser applications
+description:
+    - Manage New Relic Browser applications.
+version_added: "1.0.0"
+author:
+    - Steve Fulmer (@stevefulme1)
+options:
+    state:
+        description: Desired state of the resource.
+        type: str
+        default: present
+        choices: [present, absent]
+    host:
+        description: API host address.
+        type: str
+        required: true
+    app_id:
+        description: Unique identifier.
+        type: str
+    name:
+        description: Display name.
+        type: str
+    username:
+        description: Authentication username.
+        type: str
+    password:
+        description: Authentication password.
+        type: str
+    api_key:
+        description: API key for authentication.
+        type: str
+    validate_certs:
+        description: Validate SSL certificates.
+        type: bool
+        default: true
+"""
+
+EXAMPLES = r"""
+- name: Create
+  stevefulme1.newrelic.newrelic_browser_app:
+    host: api.example.com
+    name: my-resource
+    state: present
+
+- name: Delete
+  stevefulme1.newrelic.newrelic_browser_app:
+    host: api.example.com
+    app_id: "example-id"
+    state: absent
+"""
+
+RETURN = r"""
+browser_app:
+    description: Resource details.
+    returned: on success
+    type: dict
+"""
+
+from ansible.module_utils.basic import AnsibleModule
+
+try:
+    from ansible_collections.stevefulme1.newrelic.plugins.module_utils.api_client import ApiClient
+    HAS_CLIENT = True
+except ImportError:
+    HAS_CLIENT = False
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            state=dict(type="str", default="present", choices=["present", "absent"]),
+            app_id=dict(type="str"),
+            name=dict(type="str"),
+            host=dict(type="str", required=True),
+            username=dict(type="str"),
+            password=dict(type="str", no_log=True),
+            api_key=dict(type="str", no_log=True),
+            validate_certs=dict(type="bool", default=True),
+        ),
+        supports_check_mode=True,
+        required_if=[("state", "absent", ("app_id",))],
+    )
+
+    if not HAS_CLIENT:
+        module.fail_json(msg="Required Python libraries not found.")
+
+    client = ApiClient(module)
+    state = module.params["state"]
+    resource_id = module.params.get("app_id")
+
+    if state == "present":
+        if resource_id:
+            result = client.update("browser_app", resource_id, module.params)
+        else:
+            if module.check_mode:
+                module.exit_json(changed=True)
+            result = client.create("browser_app", module.params)
+        module.exit_json(changed=True, browser_app=result)
+    else:
+        if module.check_mode:
+            module.exit_json(changed=True)
+        client.delete("browser_app", resource_id)
+        module.exit_json(changed=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ plugins/modules/newrelic_cloud_gcp_info.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright 2026 Steve Fulmer
+# Apache-2.0 (see LICENSE)
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+"""newrelic_cloud_gcp_info module."""
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: newrelic_cloud_gcp_info
+short_description: Retrieve cloud gcp information
+description:
+    - Retrieve details about cloud gcps.
+    - Read-only module.
+version_added: "1.0.0"
+author:
+    - Steve Fulmer (@stevefulme1)
+options:
+    host:
+        description: API host address.
+        type: str
+        required: true
+    link_id:
+        description: ID of a specific resource.
+        type: str
+    name:
+        description: Filter by name.
+        type: str
+    username:
+        description: Authentication username.
+        type: str
+    password:
+        description: Authentication password.
+        type: str
+    api_key:
+        description: API key for authentication.
+        type: str
+    validate_certs:
+        description: Validate SSL certificates.
+        type: bool
+        default: true
+"""
+
+EXAMPLES = r"""
+- name: List all
+  stevefulme1.newrelic.newrelic_cloud_gcp_info:
+    host: api.example.com
+  register: result
+
+- name: Get specific
+  stevefulme1.newrelic.newrelic_cloud_gcp_info:
+    host: api.example.com
+    link_id: "example-id"
+  register: result
+"""
+
+RETURN = r"""
+cloud_gcps:
+    description: List of resource details.
+    returned: always
+    type: list
+    elements: dict
+"""
+
+from ansible.module_utils.basic import AnsibleModule
+
+try:
+    from ansible_collections.stevefulme1.newrelic.plugins.module_utils.api_client import ApiClient
+    HAS_CLIENT = True
+except ImportError:
+    HAS_CLIENT = False
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            link_id=dict(type="str"),
+            name=dict(type="str"),
+            host=dict(type="str", required=True),
+            username=dict(type="str"),
+            password=dict(type="str", no_log=True),
+            api_key=dict(type="str", no_log=True),
+            validate_certs=dict(type="bool", default=True),
+        ),
+        supports_check_mode=True,
+    )
+
+    if not HAS_CLIENT:
+        module.fail_json(msg="Required Python libraries not found.")
+
+    client = ApiClient(module)
+    resource_id = module.params.get("link_id")
+
+    if resource_id:
+        result = client.get("cloud_gcp", resource_id)
+        resources = [result] if result else []
+    else:
+        resources = client.list("cloud_gcp", module.params)
+
+    module.exit_json(changed=False, cloud_gcps=resources)
+
+
+if __name__ == "__main__":
+    main()
