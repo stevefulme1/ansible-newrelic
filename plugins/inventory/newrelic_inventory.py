fb86cb20de41d97f47bf6f4771a7dfcc0bb8b51b
sfulmer <sfulmer@redhat.com>
2026-05-16 13:08:21 -0400

    Fix inventory plugin: remove invalid plugin_type from DOCUMENTATION

--- plugins/inventory/newrelic_inventory.py
+++ plugins/inventory/newrelic_inventory.py
@@ -7,12 +7,12 @@
 """newrelic dynamic inventory plugin."""
 
 from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
 DOCUMENTATION = r"""
 ---
 name: newrelic_inventory
-plugin_type: inventory
 short_description: New Relic infrastructure hosts
 description:
     - Dynamically discovers New Relic infrastructure hosts.
@@ -59,6 +59,7 @@ from ansible.plugins.inventory import BaseInventoryPlugin
 
 try:
     import requests
+
     HAS_REQUESTS = True
 except ImportError:
     HAS_REQUESTS = False
