d7bec5b53c04a5d5d9926d77df3a36e18231e9fa
sfulmer <sfulmer@redhat.com>
2026-05-16 13:08:42 -0400

    Fix unused variable in inventory plugin

--- plugins/inventory/newrelic_inventory.py
+++ plugins/inventory/newrelic_inventory.py
@@ -82,7 +82,7 @@ class InventoryModule(BaseInventoryPlugin):
 
         host = self.get_option("host")
         api_key = self.get_option("api_key")
-        group = self.inventory.add_group("newrelic")
+        self.inventory.add_group("newrelic")
 
         # Discovery logic - connect to API and populate inventory
         try:
